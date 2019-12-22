5650407fffe079252043f0237965652e66f55ed9
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2019-12-22 11:03:12 +0100

    fix script; make deploy on all branches

--- .travis/pull.sh
+++ .travis/pull.sh
@@ -19,21 +19,21 @@ fi
 MOD_TIME=`grep last-modified headers.txt|cut -c16- - | tr -d '\r'`
 
 # convert to dot separated
-NEW_VERSION=`date -j -f '%a, %d %b %Y %T %Z' $MOD_TIME '+%Y.%m.%d'`
+NEW_VERSION=`date -j -f '%a, %d %b %Y %T %Z' "$MOD_TIME" '+%Y.%m.%d'`
 
 # get old version number
 OLD_VERSION=`cat VERSION | tr -d "\n"`
 
-if [ $NEW_VERSION = $OLD_VERSION ]; then
+if [ "$NEW_VERSION" = $OLD_VERSION ]; then
   # second run this day
   NEW_VERSION="$NEW_VERSION-1"
-elif [ $NEW_VERSION = ${OLD_VERSION[1,10]} ]; then
+elif [ "$NEW_VERSION" = "${OLD_VERSION[1,10]}" ]; then
   num=${OLD_VERSION[12,${#OLD_VERSION}]}
   ((num = $num + 1))
   NEW_VERSION=${NEW_VERSION}-${num}
 fi
 
-echo $OLD_VERSION '>' $NEW_VERSION
+echo $OLD_VERSION '=>' $NEW_VERSION
 
 echo $NEW_VERSION > VERSION
 export VERSION=$NEW_VERSION
